ef73a3a77a509b2371bba273966132a4ebbb98de
Samory Sundjata <samory.sundjata@gmail.com>
2020-11-30 12:02:27 -0300

    Main.js adicionado.

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/*
+var fruta = {nome: "maçã", cor: "vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+
+var lista = ["maçã", "pêra", "laranja"];
+lista.push = ("uva");
+lista.pop();
+console.log(lista);
+
+var nome = "Samory Sundjata";
+var idade = 51;
+alert("Bem vindo " + nome + "que tem a idade de " + idade + " anos!");
+console.log(nome);
+*/
